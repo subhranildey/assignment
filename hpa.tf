a06d578ba190289d420a0503c82183fe00f70937
Subhranil Dey <contact.subhranil.dey@gmail.com>
2020-05-26 00:46:22 +0530

    setup horizonta_pod_autoscaler | hpa.tf

---
+++ hpa.tf
@@ -0,0 +1,39 @@
+resource "kubernetes_horizontal_pod_autoscaler" "hpa" {
+  metadata {
+    name      = var.namespace
+    namespace = var.namespace
+  }
+
+  spec {
+    min_replicas = 2
+    max_replicas = 5
+
+    scale_target_ref {
+      api_version = "apps/v1"
+      kind        = "Deployment"
+      name        = var.namespace
+    }
+
+    metric {
+      type = "Resource"
+      resource {
+        name = "memory"
+        target {
+          type                = "Utilization"
+          average_utilization = 60
+        }
+      }
+    }
+    metric {
+      type = "Resource"
+      resource {
+        name = "cpu"
+        target {
+          type                = "Utilization"
+          average_utilization = 50
+        }
+      }
+    }
+  }
+}
+
